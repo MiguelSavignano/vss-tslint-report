cab9d939219d5acc108e9943ce1398f927dd39b4
miguel savignano <miguelsavignano@localhost.localdomain>
2019-06-16 15:19:19 +0200

    add workDir and filePath inputs

--- buildAndReleaseTask/index.ts
+++ buildAndReleaseTask/index.ts
@@ -1,13 +1,15 @@
 import tl = require("azure-pipelines-task-lib/task");
+import * as fs from "fs";
 
 async function run() {
   try {
-    const inputString: string = tl.getInput("samplestring", true);
-    if (inputString == "bad") {
-      tl.setResult(tl.TaskResult.Failed, "Bad input was given");
-      return;
-    }
-    console.log("Hello", inputString);
+    const workDir: string = tl.getInput("workDir", true);
+    const tslintResultFilePath: string = tl.getInput("filePath", true);
+    console.log("********", workDir, tslintResultFilePath, "*************");
+    const fileText = fs.readFileSync(`${workDir}/${tslintResultFilePath}`, {
+      encoding: "utf8"
+    });
+    console.log("File", fileText);
   } catch (err) {
     tl.setResult(tl.TaskResult.Failed, err.message);
   }
